317e601897dcd307f878913b37dad7e69cffbf17
Siddharth Bhat <siddu.druid@gmail.com>
2021-02-15 03:23:11 +0530

    add whatever else nonsense I have lying around

--- card-draw-trick.py
+++ card-draw-trick.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import random
-random.seed(12357)
+import time
+random.seed(253)
 deck = list(range(13*4))
 random.shuffle(deck)
 
@@ -8,4 +9,4 @@ suits = ["♠", "♡", "♣" , "♢"]
 numbers = [str(s) for s in range(1, 11)] + ["J(5)", "Q(5)", "K(5)", "A(5)"] 
 for card in deck:
     print("%s %s" % (suits[card//13], numbers[card % 13]))
-    input()
+    time.sleep(3)
--- makefile
+++ makefile
@@ -1,6 +1,9 @@
 # https://clojure.org/guides/deps_and_cli
 .PHONY: run
 
+tensor.out: 1-tensor.cpp
+	clang++ 1-tensor.cpp -o tensor.out -Wall -Werror -fsanitize=address -fsanitize=undefined
+
 repl:
 	clj -m nrepl.cmdline \
     --middleware "[cider.nrepl/cider-middleware]" \
